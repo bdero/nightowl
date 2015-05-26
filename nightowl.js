ec73adfa3226c15d7fa507c538f542d9882610cb
Brandon DeRosier <bdero@mit.edu>
2015-05-26 19:15:25 -0400

    Add shell

--- nightowl.js
+++ nightowl.js
@@ -3,7 +3,19 @@ Usage:\
   nightowl.js shell\
 "
 var docopt = require('docopt').docopt
+var args = docopt(doc, { version: '0.0.1' })
 
-console.log(
-  docopt(doc, { version: '0.0.1' })
-)
+if (args.shell) {
+  run_shell();
+}
+
+function run_shell() {
+  var shell = require('shell');
+
+  var app = shell();
+  app.configure(function() {
+    app.use(shell.history({ shell: app }));
+    app.use(shell.completer({ shell: app }));
+    app.use(shell.help({ shell: app, instruction: true }));
+  });
+}
